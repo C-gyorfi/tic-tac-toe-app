ad6f30d9d1e6588827e0c6df85627092e56c4534
Csabi <c.gyorfi.audio@gmail.com>
2019-02-22 23:10:38 +0000

    refactored

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -10,13 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // 1 is O, 2 is X
+    var isActiveGame = true
     
-    var activeGame = true
+    var moves = 0
     
-    var steps = 0
-    
-    var activePlayer = 1
+    var activePlayer = 1 //1 is O 2 is X
     
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] // 0 -empty, 1 - Noughts, 2 - Crosses
     
@@ -25,95 +23,78 @@ class ViewController: UIViewController {
     @IBOutlet weak var winningLabel: UILabel!
     @IBOutlet weak var playAgainButton: UIButton!
     
-
     @IBAction func buttonPressed(_ sender: UIButton) {
-        
         
         let activePosition = sender.tag - 1
         
-        if gameState[activePosition] == 0 && activeGame {
+        if gameState[activePosition] == 0 {
             
             gameState[activePosition] = activePlayer
-            
-            steps += 1
-            print(steps)
+            moves += 1
         
             if activePlayer == 1 {
                 
                 sender.setImage(UIImage(named: "O.png"), for: [])
                 activePlayer = 2
-            
+
             } else {
         
                 sender.setImage(UIImage(named: "X.png"), for: [])
                 activePlayer = 1
-        
             }
             
-            for combination in winningCombinations {
-            
-                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
-                    
-                        activeGame = false
-                    
-                    
-                        winningLabel.isHidden = false
-                        playAgainButton.isHidden = false
-                    
-                    
-                        if gameState[combination[0]] == 1 {
-                        winningLabel.text = "Noughts has won!"
-                        } else {
-                    
-                        winningLabel.text = "Crosses has won!"
-                            
-                        }
-                    
-                    UIView.animate(withDuration: 1, animations: {
-                        self.winningLabel.center = CGPoint(x: self.winningLabel.center.x + 500, y: self.winningLabel.center.y)
-                        self.playAgainButton.center = CGPoint(x: self.playAgainButton.center.x, y: self.playAgainButton.center.y + 1000)
-                    })
-                    
-                }
-                
-                else if steps == 9 && activeGame {
-                
-                    winningLabel.isHidden = false
-                    playAgainButton.isHidden = false
-                    
-                    winningLabel.text = "it's a tie..."
-                    
-                    UIView.animate(withDuration: 1, animations: {
-                        self.winningLabel.center = CGPoint(x: self.winningLabel.center.x + 500, y: self.winningLabel.center.y)
-                        self.playAgainButton.center = CGPoint(x: self.playAgainButton.center.x, y: self.playAgainButton.center.y + 1000)
-                    })
-
-                
-                }
-                
-            
-            }
-            
+            checkForWinner()
         }
-        
+    }
+    
+    private func checkForWinner() {
+        for combination in winningCombinations {
+            
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                
+                isActiveGame = false
+                
+                winningLabel.isHidden = false
+                playAgainButton.isHidden = false
+                
+                if gameState[combination[0]] == 1 {
+                    winningLabel.text = "Noughts has won!"
+                } else {
+                    
+                    winningLabel.text = "Crosses has won!"
+                }
+                
+                UIView.animate(withDuration: 1, animations: {
+                    self.winningLabel.center = CGPoint(x: self.winningLabel.center.x + 500, y: self.winningLabel.center.y)
+                    self.playAgainButton.center = CGPoint(x: self.playAgainButton.center.x, y: self.playAgainButton.center.y + 1000)
+                })
+                
+            }
+                
+            else if isBoardFull() && isActiveGame {
+                winningLabel.text = "it's a tie..."
+                winningLabel.isHidden = false
+                playAgainButton.isHidden = false
+                
+                UIView.animate(withDuration: 1, animations: {
+                    self.winningLabel.center = CGPoint(x: self.winningLabel.center.x + 500, y: self.winningLabel.center.y)
+                    self.playAgainButton.center = CGPoint(x: self.playAgainButton.center.x, y: self.playAgainButton.center.y + 1000)
+                })
+            }
+        }
     }
     
     @IBAction func playAgainButton(_ sender: UIButton) {
         
-        activeGame = true
-        
-        steps = 0
-        
+        isActiveGame = true
+        moves = 0
         activePlayer = 1
-        
         gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] // 0 -empty, 1 - Noughts, 2 - Crosses
         
         for n in 1..<10 {
         
             if let button = view.viewWithTag(n) as? UIButton {
-            
                 button.setImage(nil, for: [])
-            
             }
             
             self.playAgainButton.isHidden = true
@@ -121,28 +102,20 @@ class ViewController: UIViewController {
             
             winningLabel.center = CGPoint(x: winningLabel.center.x - 500, y:winningLabel.center.y)
             playAgainButton.center = CGPoint(x: playAgainButton.center.x, y:playAgainButton.center.y-500)
-
-            
         }
-        
+    }
+    
+    private func isBoardFull() -> Bool {
+        return moves == 9
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
         self.playAgainButton.isHidden = true
         self.winningLabel.isHidden = true
         
         winningLabel.center = CGPoint(x: winningLabel.center.x - 500, y:winningLabel.center.y)
         playAgainButton.center = CGPoint(x: playAgainButton.center.x, y:playAgainButton.center.y-500)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
